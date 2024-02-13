ee582cbfef2e411e7911565f8cb175d1a915083f
PontusAL <ponat404@student.liu.se>
2024-02-13 12:04:55 +0100

    Added additional imports

--- server/main.py
+++ server/main.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 from flask import Flask
 from flask import jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask import request
+from flask_bcrypt import Bcrypt
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../client', static_url_path='/')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+bcrypt = Bcrypt(app)
+db = SQLAlchemy(app)
+
 
 @app.route('/hello')
 def hello():
