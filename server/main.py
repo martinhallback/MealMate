6d5cd918fd2109af2c70429d4368f0583cc2cb44
PontusAL <ponat404@student.liu.se>
2024-03-04 11:27:21 +0100

    divided classes into files

--- server/main.py
+++ server/main.py
@@ -4,6 +4,9 @@ from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask import request
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+
+
 
 app = Flask(__name__, static_folder='../client', static_url_path='/')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -11,6 +14,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
 
+#Project files
+import userClasses
 
 @app.route('/hello')
 def hello():
