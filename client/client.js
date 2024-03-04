588c939e02a8df0b1aa532227ae52f7efb0e65de
Martin Hallbäck <martin.hallback@perimed-instruments.com>
2024-03-04 18:47:08 +0100

    started .js structure

--- client/client.js
+++ client/client.js
@@ -1,3 +1,21 @@
 $(document).ready(function () {
+
+
+    $('li.nav-item a.nav-link').click(function (e) {
+        e.preventDefault();
+        var linkText = $(this).text();
+
+        switch (linkText) {
+            case 'Sell':
+              
+              break;
+            case 'Contact':
+              
+              break;
+            
+            default:
+              
+          }
+    });
     
 });
