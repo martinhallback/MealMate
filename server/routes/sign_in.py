4c92449e7c664662d0568917b535d4a7761fc0d2
PontusAL <ponat404@student.liu.se>
2024-03-05 14:35:36 +0100

    fixed

--- server/routes/sign_in.py
+++ server/routes/sign_in.py
@@ -9,6 +9,8 @@ from flask_bcrypt import generate_password_hash
 from flask_jwt_extended import JWTManager
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import get_jwt_identity
+from flask import Blueprint, current_app
+
 
 
 
