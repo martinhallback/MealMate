996ab0543399889d57f1be1a38f9d0ab08315ab1
Olle Carlstedt <ollecarlstedt@mbpsomtlhorolle.students.netlogon.liu.se>
2024-03-07 12:17:01 +0100

    added ajax calls to signup and login

--- client/client.js
+++ client/client.js
@@ -15,13 +15,10 @@ $(document).ready(function () {
               break;
             
             case 'Sign up':
-              console.log('SIngup');
               loadSignUpContent();
-
             break;
 
             case 'Log in':
-              console.log('Klick');
             loadLogInContent();
             break;
 
@@ -89,14 +86,16 @@ function foodAdModal(card){
 function loadSignUpContent() {
   console.log('Load Sign Up Content');
   $("#signUpContainer").load("signup.html #signup-modal", function () {
-
     showSignUpModal();
   });
 }
 
 function showSignUpModal(){
-  console.log('showsignup');
   $('#signup-modal').modal('show');
+  $("#signUpBtn").off().on('click', function (e) {
+    console.log('Signup klickat');
+    postNewUser();
+  });
 }
 
 function loadLogInContent() {
@@ -109,5 +108,54 @@ function loadLogInContent() {
 function showLogInModal(){
   console.log('showlogin');
   $('#login-modal').modal('show');
+  $("#logInBtn").off().on('click', function (e) {
+    console.log('Login');
+    logInUser();
+  });
 }
+
+function postNewUser() {
+ // e.preventDefault();
+  var host = window.location.protocol + '//' + location.host
+  newUserEmail = document.getElementById('email').value;
+  newUserName = document.getElementById('name').value;
+  newUserPassword = document.getElementById('password').value;
+  $.ajax({
+    url: host + '/sign-up',
+    type: 'POST',
+    contentType: 'application/json',
+   // headers: {"Authorization": "Bearer " + JSON.parse(sessionStorage.getItem('auth')).token},
+    data: JSON.stringify({
+        email: newUserEmail,
+        name : newUserName,
+        password : newUserPassword,
+    }),
+    success: function() {
+      console.log('signed up');
+     $('#signup-modal').modal('hide');
+  }
+});
+}
+
+function logInUser(){
+  var host = window.location.protocol + '//' + location.host
+    var email = $("#email").val();
+    var password = $("#password").val();
+
+    $.ajax({
+        url: host + '/login',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({
+            email,
+            password,
+        }),
+        success: function(response) {
+            console.log('login succesful');
+            sessionStorage.setItem('auth', JSON.stringify(response))
+            $('#login-modal').modal('hide');
+        }
+    });
+}
+
 
