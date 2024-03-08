bcbd5d363bf094b5e8d13b20f0485ab20644277b
Martin Hallbäck <martin.hallback@perimed-instruments.com>
2024-03-08 15:58:42 +0100

    started on a footer

--- client/client.js
+++ client/client.js
@@ -1,5 +1,22 @@
 $(document).ready(function () {
   homeview();
+  $(".footer").load("footer.html", function () {});
+
+  $(window).scroll(function () {
+     // Calculate the bottom position of the window
+     var bottomWindow = $(window).scrollTop() + $(window).height();
+     // Calculate the height of the document
+     var documentHeight = $(document).height();
+
+     // Check if the bottom of the window is at the bottom of the document
+     if (bottomWindow + 10 >= documentHeight) {
+      console.log("fade in footer");
+         $('.footer').fadeIn();
+     } else {
+         $('.footer').fadeOut();
+     }
+  });
+
   $('li.nav-item a.nav-link').click(function (e) {
     e.preventDefault();
       var linkText = $(this).text();
