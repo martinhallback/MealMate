0d3c5d532e7807473efa6d2182f2d5a45dbee5aa
louer141 <louer141@su17-111.ad.liu.se>
2024-03-26 11:09:20 +0100

    Sell viev färdig

--- client/sell.js
+++ client/sell.js
@@ -2,13 +2,15 @@
 
 function sellview() {
     $('.container').empty();
-    $('.container').append('<h2 class="startSellingYourLunchboxesHere">Start selling your lunchboxes here!</h2>');
-    $('.container').append('<div class="adcontainer">' + '</div>');
+    $('.container').append('<div class="sellcontainerjs">' + '</div>');
+    $('.sellcontainerjs').append('<h2 class="startSelling">Start selling your lunchboxes here!</h2>');
+    $('.sellcontainerjs').append('<p class="benefitsText">Earn extra income by selling your homemade lunchboxes to hungry customers. <br> Share your culinary skills and delight others with your delicious creations!</p>');
+    var imageUrl = 'Images/lunchboxsellview.jpg'; // Update the image URL with your actual image
+    $('.sellcontainerjs').append('<img src="' + imageUrl + '" alt="Lunchbox" class="sellImage">');
 
     console.log('Load Sell Content');
-    $("#sellContainer").html(""); // Clear the sellContainer
     // Append a button to initiate the selling process
-    $('.container').append('<button id="sellBtn" class="btn btn-primary">Sell</button>');
+    $('.sellcontainerjs').append('<button id="sellBtn" class="btn btn-primary">Sell</button>');
     // Attach a click event to the "Sell" button
     $("#sellBtn").off().on('click', function (e) {
         console.log('Sell button clicked');
@@ -18,9 +20,13 @@ function sellview() {
 
 // Function to show the sell form
 function showSellForm() {
-    $("#sellContainer").html(""); // Clear the sellContainer
-    $("#sellContainer").load("sell.html #sellForm", function () {
-        // You can add additional logic or event handling here if needed
+    //$(".container").empty(""); // Clear the container
+    $(".sellcontainerjs").empty();
+    $(".sellcontainerjs").load("sell.html #sellForm", function () {
+        if (status == "error") {
+            var msg = "Sorry but there was an error: ";
+            console.log(msg + xhr.status + " " + xhr.statusText);
+        }
     });
 }
     
