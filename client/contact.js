205ad574d791e384c1072aa49238c17e27ce3e87
louer141 <louer141@su17-111.ad.liu.se>
2024-03-26 14:01:00 +0100

    jobbar på contact view, inte färdig

---
+++ client/contact.js
@@ -0,0 +1,6 @@
+function sellview() {
+    
+    $('.container').empty();
+    $('.container').append('<div class="sellcontainerjs">' + '</div>');
+    $('.contactContainerjs').append('<h2 class="contactTitle">Contact Info</h2>');
+}
