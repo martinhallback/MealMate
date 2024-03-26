bc75dcd2c6777a3360af6bc580b8d42693a31001
ponat404 <ponat404@student.liu.se>
2024-03-26 13:52:42 +0100

    cannot test fuinctionality of authentification until signup creates correct PW_hash

--- server/classes/user.py
+++ server/classes/user.py
@@ -5,17 +5,17 @@ class User(object):
     def __init__(self, objID, email=None, pwHash=None, phoneNumber=None, fullName=None, 
                  PNumber=None, isVerified=None, uni=None, studentID=None, homeAddress=None, 
                  sellLocation=None, isAdmin=None):
-        id = objID
-        pw_hash = pwHash
-        name = fullName
-        phone_number = phoneNumber
-        personal_number = PNumber
-        is_verified = isVerified
-        university = uni
-        student_id = studentID
-        address = homeAddress
-        location = sellLocation
-        is_admin = isAdmin
+        self.id = objID
+        self.password_hash = pwHash
+        self.name = fullName
+        self.phone_number = phoneNumber
+        self.personal_number = PNumber
+        self.is_verified = isVerified
+        self.university = uni
+        self.student_id = studentID
+        self.address = homeAddress
+        self.location = sellLocation
+        self.is_admin = isAdmin
 
     def set_password(self, password):
         self.password_hash = bcrypt.generate_password_hash(password).decode('utf8')
--- server/routes/login.py
+++ server/routes/login.py
@@ -4,8 +4,13 @@ from flask_jwt_extended import create_access_token
 
 from flask import Blueprint, current_app
 
-from main import bcrypt
+from main import bcrypt, db
 
+#import relevant classes
+"""import sys
+sys.path.append('../classes/')
+from user import User """
+from classes import user
 
 bp = Blueprint('login', __name__)
 
@@ -13,6 +18,25 @@ bp = Blueprint('login', __name__)
 
 @bp.route('/login' , methods=['POST'])
 def login():
-    #Just send an authorisation token for now before log-in functionality has been implemented
-        
-    return jsonify({"error": "Functionality has not yet been implemented"}), 401
+    data = request.get_json()
+    if not all (key in data for key in ['email', 'password']):
+        return jsonify({'error': "Must enter both email and password"}), 400
+    auth = {}
+    email = data['email']
+    
+    #getting the correct user
+    users = db["user"]
+    cursor = users.find_one({'email' : email})
+    if cursor is None:
+        return jsonify({'error': "no user with this email", 'errorCode' : 1}), 401
+    query = dict(cursor)
+    print(query)
+    pw_hash = query['PW_hash']
+    #Checking pw_hash
+    usr = user.User(query['_id'], email=email, pwHash=pw_hash)
+    if (usr.check_password(data['password'])):
+        auth['token'] = usr.create_access_token(identity=email)
+        auth['user'] = usr.serialize()
+        return jsonify({'authentification': auth}), 200
+    else:
+        return jsonify({'error' : "Incorrect password", 'errorCode' : 2}), 401
