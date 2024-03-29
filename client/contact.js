6ed68de9f608114194ff8be4eb491c1dd1287649
Louise <louer141@student.liu.se>
2024-03-29 17:05:29 +0100

    Done with helpview, aka contact view, stylized and added some details, v2

--- client/contact.js
+++
@@ -1,6 +0,0 @@
-function sellview() {
-    
-    $('.container').empty();
-    $('.container').append('<div class="sellcontainerjs">' + '</div>');
-    $('.contactContainerjs').append('<h2 class="contactTitle">Contact Info</h2>');
-}
