e21366ca5ad103eed745d7f72064506fa631abb1
Olle Carlstedt <ollca224@student.liu.se>
2024-03-31 16:58:08 +0200

    navbar development and new icons

--- client/client.js
+++ client/client.js
@@ -1,13 +1,22 @@
 $(document).ready(function () {
+  $(".footer").load("footer.html");
   homeview();
-  $(".footer").load("footer.html", function () {});
   //$('.footer').fadeIn();
   $(window).scrollTop(0);
+
+  $(".navbar-basket").click(function (e) {
+    console.log('Basket');
+    
+  });
+  $(".navbar-profile").click(function (e) {
+    console.log('Profile');
+    
+  });
   
 
   //$('li.nav-item a.nav-link').click(function (e) {
-  $('li.nav-item a.nav-link, .navbar-brand.larger-text').click(function (e) {
-    e.preventDefault();
+    $('li.nav-item a.nav-link, .navbar-brand.larger-text, .navbar-basket.ml-auto').click(function (e) {
+      e.preventDefault();
       var linkText = $(this).text();
       switch (linkText) {
         case 'MealMate':
@@ -28,7 +37,9 @@ $(document).ready(function () {
         case 'Log in':
           loadLogInContent();
           break;
-
+        
+        case 'navbar-basket-button':
+        console.log('Basket1');
         default:
               
       }
