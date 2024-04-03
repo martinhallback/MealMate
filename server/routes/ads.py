f6ee7e971b6af46bbd60215549423b843d352b1c
ponat404 <ponat404@student.liu.se>
2024-04-03 14:35:33 +0200

    Added a get for all ads in the database

--- server/routes/ads.py
+++ server/routes/ads.py
@@ -1,7 +1,6 @@
 from flask import Blueprint
 from flask import jsonify
 from flask import request
-from bson.objectid import ObjectId
 
 
 from main import db
@@ -16,7 +15,26 @@ def ads():
     # data = request.get_json() #For other requyests than get
     ads = db['advertisement']
     cursor = ads.find({})
-
+    if cursor is None:
+        return jsonify({'error': "Database collection could not be accessed ", 'errorCode' : 31}), 503
+    query = list(cursor)
+    print(query)
+    advertisements = []
+    print(query)
+    for advert in query:
+        ad = dict(advert)
+        try:
+            advertisements.append(advertisement.Advertisement(
+                objID=ad['_id'], dishName=ad['dishName'],
+                description=ad['description'],cookDate=ad['cookDate'],
+                quantity=ad['quantity'], imagePath=ad['imagePath'], 
+                sellerID=ad['sellerID']
+                ))
+        except Exception as e:
+            print(e) 
+    json_ads = [advert.serialise_existing() for advert in advertisements]
+    
+    return jsonify(json_ads), 200
 
 
     #Should return a list of all current ads, with the information: image, title, description and seller
