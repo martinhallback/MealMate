8850a85a0921507be4baea2dc31ee8130b40ae88
ponat404 <ponat404@student.liu.se>
2024-04-03 11:32:16 +0200

    Added structure for class

---
+++ server/classes/advertisement.py
@@ -0,0 +1,18 @@
+class Advertisement(object):
+    
+    
+    def __init__(self, objID=None, dishName=None, description=None, cookDate=None, quantity=None, 
+                 portionPrice=None, imagePath=None ,sellerID=None):
+        self.id = objID
+        self.dish_name = dishName
+        self.desc = description
+        self.cook_date = cookDate
+        self.quantity = quantity
+        self.portion_price = portionPrice
+        self.image_path = imagePath
+        self.seller_id = sellerID
+    
+    def serialize(self):
+        #Currently does not include IDs, wait until DB sprint to implement
+        return dict(dishName=self.dish_name, description=self.dish_name, cookDate=self.cook_date,
+                    quantity=self.quantity, portionPrice=self.portion_price, imagePath=self.image_path)
