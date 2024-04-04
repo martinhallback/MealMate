e1502abec2f571ee01ada72ca83c93dc0e87573a
Martin Hallbäck <martin.hallback@perimed-instruments.com>
2024-04-04 12:57:47 +0200

    started on ajax call

--- client/homeview.js
+++ client/homeview.js
@@ -1,61 +1,25 @@
 function homeview(){
+  
   console.log("homeview")
     $('.container').empty();
     $('.container').append('<h2 class="foodNearMe">Food near me</h2>');
     $('.container').append('<div class="adcontainer">' + '</div>');
-  
-    var cardData = [ // remove when list from backend is finished
-      { 
-          imgSrc: 'Images/TestFoodImage.jpg',
-          title: 'Card Title 1',
-          text: 'Some quick example text for card 1.',
-          extraInfo: 'Some extra info for card 1',
-          seller: {
-            name: 'Seller 1',
-            rating: '4',
-            reviews: '14'
-        }
-      },
-      { 
-          imgSrc: 'Images/TestFoodImage.jpg',
-          title: 'Card Title 2',
-          text: 'Some quick example text for card 2.',
-          extraInfo: 'Some extra info for card 2',
-          seller: {
-            name: 'Seller 2',
-            rating: '3',
-            reviews: '1'
-        }
-      },
-      { 
-        imgSrc: 'Images/TestFoodImage.jpg',
-        title: 'last card',
-        text: 'Some quick example text for last card',
-        extraInfo: 'Some extra info for last card',
-        seller: {
-          name: 'Seller 2',
-          rating: '3',
-          reviews: '1'
-        }
-      },
-      
-    ];
     
-    $.each(cardData, function(index, card) {
-      var cardHtml = '<div class="card">' +
-                          '<div class="card-body">' +
-                              '<img src="' + card.imgSrc + '" class="card-img-top" alt="...">' +
-                              '<h5 class="card-title">' + card.title + '</h5>' +
-                              '<p class="card-text">' + card.text + '</p>' +
-                              '<button type="button" class="btn btn-primary buy-btn" data-toggle="modal" data-target="#myModal_' + index + '">Buy</button>' +
-                          '</div>' +
-                      '</div>';
-  
-      $('.adcontainer').append(cardHtml);
-      var modal = foodAdModal(card, index);
-      $('.container').append(modal);
-        
+    getAds(function(cardData){
+      console.log(cardData);
+      if(cardData){
+        $.each(cardData, function(index, card) {
+          var cardHtml = createCard(index, card);
+          $('.adcontainer').append(cardHtml);
+          var modal = foodAdModal(card, index);
+          $('.container').append(modal);
+        });
+      }else{
+        console.log("no ads exist")
+      }
+      
     });
+
     $('.container').on('click', '.buy-btn', function() {
         var modalIndex = $(this).data('target').split('_')[1];
         $('#myModal_' + modalIndex).modal('show');
@@ -67,40 +31,42 @@ function homeview(){
         $(this).closest('.modal').modal('hide');
     });
 }
-  
-  
+function createCard(index, card){
+  var cardHtml = '<div class="card">' +
+                          '<div class="card-body">' +
+                              //'<img src="' + card.imgPath + '" class="card-img-top" alt="...">' +
+                              '<h5 class="card-title">' + card.title + '</h5>' +
+                              '<p class="card-text">' + card.description + '</p>' +
+                              '<p>Price: ' + card.portionPrice + '</p>' +
+                              '<button type="button" class="btn btn-primary buy-btn" data-toggle="modal" data-target="#myModal_' + index + '">Buy</button>' +
+                          '</div>' +
+                      '</div>';
+}
 function foodAdModal(card, index){
-    var starsHtml = '';
-    for (var i = 0; i < card.seller.rating; i++) {
-        starsHtml += '<span class="star filled">&#9733;</span>';
-    }
-    for (var j = card.seller.rating; j < 5; j++) {
-        starsHtml += '<span class="star">&#9734;</span>';
-    }
-
-    var modalHtml = '<div class="modal fade" id="myModal_' + index + '" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">' +
-        '<div class="modal-dialog">' +
-        '<div class="modal-content">' +
-        '<div class="modal-header">' +
-        '<h5 class="modal-title" id="exampleModalLabel">' + card.title + '</h5>' +
-        '<button type="button" class="close-btn" data-bs-dismiss="modal" aria-label="Close"></button>' +
-        '</div>' +
-        '<div class="modal-body">' +
-        '<img src="' + card.imgSrc + '" class="modal-img" alt="Food Image">' +
-        '<p>' + card.text + '</p>' +
-        '<p>Seller: ' + card.seller.name + '</p>' +
-        '<p>Rating: ' + starsHtml + ' ' + card.seller.reviews + ' Reviews' + '</p>' +
-        '<p>' + card.extraInfo + '</p>' +
-        '</div>' +
-        '<div class="modal-footer">' +
-        '<button type="button" class="btn btn-secondary close-btn" data-dismiss="modal">Close</button>' +
-        '<button type="button" class="btn btn-primary add-to-cart-btn" onclick="addtocart('+ index +')">Add to Shopping Cart</button>' +
-        '</div>' +
-        '</div>' +
-        '</div>' +
-        '</div>';
-    
-    return modalHtml;
+  var modalHtml = '<div class="modal fade" id="myModal_' + index + '" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">' +
+      '<div class="modal-dialog">' +
+      '<div class="modal-content">' +
+      '<div class="modal-header">' +
+      '<h5 class="modal-title" id="exampleModalLabel">' + card.dishName + '</h5>' +
+      '<button type="button" class="close-btn" data-bs-dismiss="modal" aria-label="Close"></button>' +
+      '</div>' +
+      '<div class="modal-body">' +
+      //'<img src="' + card.imagePath + '" class="modal-img" alt="Food Image">' +
+      '<p>' + card.description + '</p>' +
+      '<p>Cook Date: ' + card.cookDate + '</p>' +
+      '<p>Quantity: ' + card.quantity + '</p>' +
+      '<p>Price: ' + card.portionPrice + '</p>' +
+      '<p>Seller: ' + card.sellerID + '</p>' +
+      '</div>' +
+      '<div class="modal-footer">' +
+      '<button type="button" class="btn btn-secondary close-btn" data-dismiss="modal">Close</button>' +
+      '<button type="button" class="btn btn-primary add-to-cart-btn" onclick="addtocart('+ index +')">Add to Shopping Cart</button>' +
+      '</div>' +
+      '</div>' +
+      '</div>' +
+      '</div>';
+  
+  return modalHtml;
 }
 
 function addtocart(index){
@@ -116,4 +82,21 @@ function addtocart(index){
   }
  
   $('#myModal_' + index).modal('hide');
+}
+
+function getAds(callback){
+  var host = window.location.protocol + '//' + location.host
+  $.ajax({
+    url: host + '/ads',
+    type: 'GET',
+    contentType: 'application/json',
+    success: function(response){
+      console.log("fetched all the ads");
+      callback(response);
+    },
+    error: function(JQxhr, status, error){
+      console.log(error);
+      callback(null);
+    }
+  });
 }
