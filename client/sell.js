252a24f4d3992fba2874156632551e787e78cd51
louer141 <louer141@su15-112.ad.liu.se>
2024-04-05 13:13:41 +0200

    Started on fixing the color scheme of the website, will continue more

--- client/sell.js
+++ client/sell.js
@@ -4,11 +4,10 @@ function sellview() {
     
     $('.container').empty();
     $('.container').append('<div class="sellcontainerjs">' + '</div>');
+    var imageUrl = 'Images/lunchboxsellview.jpg'; //  image URL with your image
+    $('.sellcontainerjs').append('<img src="' + imageUrl + '" alt="Lunchbox" class="sellImage">');
     $('.sellcontainerjs').append('<h2 class="startSelling">Start selling your lunchboxes here!</h2>');
     $('.sellcontainerjs').append('<p class="benefitsText">Earn extra income by selling your homemade lunchboxes to hungry customers. <br> Share your culinary skills and delight others with your delicious creations!</p>');
-    var imageUrl = 'Images/lunchboxsellview.jpg'; // Update the image URL with your actual image
-    $('.sellcontainerjs').append('<img src="' + imageUrl + '" alt="Lunchbox" class="sellImage">');
-
     console.log('Load Sell Content');
     // Append a button to initiate the selling process
     $('.sellcontainerjs').append('<button id="sellBtn" class="btn btn-primary">Sell</button>');
