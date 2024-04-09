f205de586546fa43af3f67932def891587e2c32f
ponat404 <ponat404@student.liu.se>
2024-04-09 13:17:40 +0200

    Delete functionality implemented, not tested

--- server/routes/ad.py
+++ server/routes/ad.py
@@ -13,7 +13,7 @@ from classes import advertisement, user
 
 bp = Blueprint('ad', __name__)
 
-@bp.route('/ad/<string:obj_id>', methods = ['GET', 'PUT'])
+@bp.route('/ad/<string:obj_id>', methods = ['GET', 'PUT', 'DELETE'])
 def ad(obj_id):
     # data = request.get_json() #For other requests than get
     if request.method == 'GET':
@@ -24,7 +24,6 @@ def ad(obj_id):
         if cursor is None:
             return jsonify({'error': "No advertisement with that id"}), 404
         query = dict(cursor)
-        print(query)
         try: 
             advert = advertisement.Advertisement(query)
             return jsonify(advert.serialise_client()), 200
@@ -33,6 +32,27 @@ def ad(obj_id):
             return jsonify({'error': "Advert object in database was invalid", 'errorCode' : 41}), 503
     if request.method == 'PUT':
         return jsonify({'error' : "functionality not yet implemented", 'errorCode' : 0}), 401
+    if request.method == 'DELETE':
+        ads = db['advertisement']
+        objID = ObjectId(obj_id)
+        cursor = ads.find_one({'_id' : objID})
+        if cursor is None:
+            return jsonify({'error': "No advertisement with that id"}), 404
+        query = dict(cursor)
+        data = request.get_json()
+        if data is None or 'user' not in data:
+            return jsonify({'error' : "user object from authentification must be included", 'errorCode' : 5}), 401
+        try:
+            usr = user.User(data['user'])
+        except Exception as e:
+            print("Contact backend!!!\n", e)
+            return jsonify({'error' : "Your user is invalid"}), 401
+        
+        delete_result = ads.delete_one({'_id' : objID})
+        print(delete_result)
+        return jsonify({'success' : "Your advertisement has been deleted"}), 200
+        
+
 
 @bp.route('/ad', methods=['POST'])
 #@jwt_required()
