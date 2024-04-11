ebfc199e030655b71f6cbda8c58f5cec966862cc
ponat404 <ponat404@student.liu.se>
2024-04-11 15:41:00 +0200

    validation rules cleanup, almost done

--- server/dbSetup.py
+++ server/dbSetup.py
@@ -129,7 +129,7 @@ allergy_validation = {
     }
 }
 if __name__ == "__main__":
-    db.command("collMod", "user",  validator=allergy_validation)
+    db.command("collMod", "allergy",  validator=allergy_validation)
 
 
 #initialising fields in collection
@@ -140,7 +140,6 @@ allergy.insert_one({"type" : "laktos"})"""
 
 
 #Protein collection
-protein = db["protein"]
 validation_rule = {
     "$jsonSchema" : {
         "bsonType" : "object",
@@ -157,11 +156,13 @@ validation_rule = {
         }
     }
 }
+if __name__ == "__main__":
+    db.command("collMod", "protein",  validator=allergy_validation)
 #initialiser object
 
 #Location
 location = db["location"]
-validation_rule = {
+location_validation = {
     "$jsonSchema" : {
         "bsonType" : "object",
         "required" : ["city", "area"],
@@ -178,162 +179,16 @@ validation_rule = {
     }
 }
 
-"""location.insert_one(validation_rule)
-location.insert_one({"city" : "Linköping", "area" : "Ryd"})"""""
-
-#Location
-
-
-"""user.create_index([("email", ASCENDING)], unique=True)
-#user.create_index([("PW_hash", ASCENDING)], unique=True)
-user.insert_one(validation_rule)"""""
-#user.insert_one({"email" : "johan.loven@gmail.com", "PW_hash" : "1234", "phoneNumber" : "0701234567", "name" : "Johan Loven", "PN" : "19990101-1234", "isVerified" : True, "university" : "Linköping University", "student_ID" : "abc123", "address" : "Storgatan 1", "location" : ObjectId("6601494cc589dfa2b4c4bf30")})
-
-
-
-#Location collection
-location = db["location"]
-validation_rule = {
-    "$jsonSchema" : {
-        "bsonType" : "object",
-        "required" : ["city", "area"],
-        "properties" : {
-            "city" : {
-                "bsonType" : "string",
-                "description" : "Name of city"
-            },
-            "area" : {
-                "bsonType" : "string",
-                "description" : "Name of area"
-            } 
-        }
-    }
-}
-
-#location.create_index([("city", ASCENDING), ("area", ASCENDING)], unique=True)
-"""location.insert_one(validation_rule)
-location.insert_one({"city" : "Linköping", "area" : "Ryd"})"""""
-
-
-
-
-#User collection
-user = db["user"]
-validation_rule = {
-    "$jsonSchema" : {
-        "bsonType" : "object",
-        "required" : ["email", "phoneNumber","pwHash", "university","studentID"],
-        "properties" : {
-            "email" : {
-                "bsonType" : "string",
-                "description" : "email of the user"
-            },
-            "phoneNumber" : {
-                "bsonType" : "string",
-                "description" : "phone number of the user"
-            },
-            "pwHash" : {
-                "bsonType" : "string",
-                "description" : "password hash"
-            },
-            "university" : {
-                "bsonType" : "string",
-                "description" : "university of the user"
-            },
-            "studentID" : {
-                "bsonType" : "string",
-                "description" : "student ID of the user"
-            }     
-        }
-    }
-}
-#user.insert_one(validation_rule)
-
-
-#Location
-location = db["location"]
-validation_rule = {
-    "$jsonSchema" : {
-        "bsonType" : "object",
-        "required" : ["city", "area"],
-        "properties" : {
-            "city" : {
-                "bsonType" : "string",
-                "description" : "Name of city"
-            },
-            "area" : {
-                "bsonType" : "string",
-                "description" : "Name of area"
-            } 
-        }
-    }
-}
+if __name__ == "__main__":
+    db.command("collMod", "location",  validator=location_validation)
 
 """location.insert_one(validation_rule)
 location.insert_one({"city" : "Linköping", "area" : "Ryd"})"""""
-
-#Location
-user = db["user"]
-validation_rule = {
-    "$jsonSchema" : {
-        "bsonType" : "object",
-        "required" : ["email", "PW_hash", "phoneNumber", "name", "PN", "isVerified", "university", "student_ID", "address", "location"],
-        "properties" : {
-            "email" : {
-                "bsonType" : "string",
-                "description" : "Email of user"
-            },
-            "PW_hash" : {
-                "bsonType" : "string",
-                "description" : "Hashed password"
-            },
-             "phoneNumber" : {
-                "bsonType" : "string",
-                "description" : "Phone number of user"
-            },
-            "name" : {
-                "bsonType" : "string",
-                "description" : "Name of user"
-            },
-            "PN" : {
-                "bsonType" : "string",
-                "description" : "Personal number of user"
-            },
-             "isVerified" : {
-                "bsonType" : "bool",
-                "description" : "If user is verified"
-            },
-             "university" : {
-                "bsonType" : "string",
-                "description" : "University of user"
-            },
-             "student_ID" : {
-                "bsonType" : "string",
-                "description" : "Student ID of user"
-            },
-             "address" : {
-                "bsonType" : "string",
-                "description" : "Address of user"
-            },
-             "location" : {
-                "bsonType" : "objectId",
-                "description" : "Location of user"
-            },
-        }
-    }
-}
-"""user.create_index([("email", ASCENDING)], unique=True)
-#user.create_index([("PW_hash", ASCENDING)], unique=True)
-user.insert_one(validation_rule)"""""
-#user.insert_one({"email" : "johan.loven@gmail.com", "PW_hash" : "1234", "phoneNumber" : "0701234567", "name" : "Johan Loven", "PN" : "19990101-1234", "isVerified" : True, "university" : "Linköping University", "student_ID" : "abc123", "address" : "Storgatan 1", "location" : ObjectId("6601494cc589dfa2b4c4bf30")})
-
-
 
 
 
 # Advertisement collection
-ad = db["advertisement"]
-validation_rule = {
+advert_validation = {
     "$jsonSchema" : {
         "bsonType" : "object",
         "required" : ["dishName", "description", "cookDate", "quantity", "portionPrice", "imagePath", "sellerID"],
@@ -369,63 +224,12 @@ validation_rule = {
         }
     }
 }
-
-#ad.insert_one(validation_rule)
-#ad.insert_one({"dishName" : "Köttbullar", "description" : "Bra mat", "cookDate" : datetime.datetime(2021,5,1), "quantity" : 5, "portionPrice" : 20.0, "imagePath" : "image.jpg", "sellerID" : ObjectId("60f0b1b3c589dfa2b4c4bf2f")})
-
-
-
-# Ad_contains_protein collection
-ad_contains_protein = db["ad_contains_protein"]
-validation_rule = {
-    "$jsonSchema" : {
-        "bsonType" : "object",
-        "required" : ["advertisement", "protein"],
-        "properties" : {
-            "advertisement" : {
-                "bsonType" : "objectId",
-                "description" : "Id of advertisement"
-            },
-            "protein" : {
-                "bsonType" : "objectId",
-                "description" : "Type of protein"
-            } 
-        }
-    }
-}
-
-#ad_contains_protein.create_index([("advertisement", ASCENDING), ("protein", ASCENDING)], unique=True)
-#ad_contains_protein.insert_one(validation_rule)
-"""ad_contains_protein.insert_one({"advertisement" : ObjectId("660153bed6271ac7cda390aa"), "protein" : ObjectId("65f58feaa5d9518927339597")})"""""
-
-
-# Ad_contains_allergy collection
-ad_contains_allergy= db["ad_contains_allergy"]
-validation_rule = {
-    "$jsonSchema" : {
-        "bsonType" : "object",
-        "required" : ["advertisement", "allergy"],
-        "properties" : {
-            "advertisement" : {
-                "bsonType" : "objectId",
-                "description" : "Id of advertisement"
-            },
-            "allergy" : {
-                "bsonType" : "objectId",
-                "description" : "Type of allergy"
-            } 
-        }
-    }
-}
-
-#ad_contains_allergy.create_index([("advertisement", ASCENDING), ("allergy", ASCENDING)], unique=True)
-#ad_contains_allergy.insert_one(validation_rule)
-#ad_contains_allergy.insert_one({"advertisement" : ObjectId("660153bed6271ac7cda390aa"), "allergy" : ObjectId("65f58b8a760b3ad4e3459265")})
+#if __name__ == "__main__":
+    #db.command("collMod", "advertisement",  validator=advert_validation)
 
 
 # Purchase collection
-purchase = db["purchase"]
-validation_rule = {
+purchase_validation = {
     "$jsonSchema" : {
         "bsonType" : "object",
         "required" : ["date", "totalPrice", "quantity", "sellerRating", "reviewText", "buyer", "seller", "advertisement"],
@@ -465,13 +269,6 @@ validation_rule = {
         }
     }
 }
-#purchase.insert_one(validation_rule)
-"""purchase.insert_one({"date" : datetime.datetime(2021,5,1) ,
-                    "totalPrice" : 20.0, 
-                    "quantity" : 5, 
-                    "sellerRating" : 4.5, 
-                    "reviewText" : "Bra mat", 
-                    "buyer" : ObjectId("6601539871f145239905fc1a"), 
-                    "seller" : ObjectId("6601539871f145239905fc1a"),
-                    "advertisement" : ObjectId("660153bed6271ac7cda390aa")})"""
+#if __name__ == "__main__":
+    #db.command("collMod", "purchase",  validator=purchase_validation)
 
