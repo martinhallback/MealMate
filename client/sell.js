fa7bf630c2f45b5f12e003dc4c0e5557a41b82fd
Martin Hallbäck <martin.hallback@perimed-instruments.com>
2024-04-11 11:46:21 +0200

    cont the functionality for posting ads

--- client/sell.js
+++ client/sell.js
@@ -9,7 +9,7 @@ function sellview() {
     $('.sellcontainerjs').append('<h2 class="startSelling">Start selling your lunchboxes here!</h2>');
     $('.sellcontainerjs').append('<p class="benefitsText">Earn extra income by selling your homemade lunchboxes to hungry customers. <br> Share your culinary skills and delight others with your delicious creations!</p>');
     console.log('Load Sell Content');
-    // Append a button to initiate the selling process
+
     $('.sellcontainerjs').append('<button id="sellBtn" class="btn btn-primary">Sell</button>');
     $('.container').append('<div id="SellQuestionsContainer">' +
         '<h3 class="ifQuestionsTitle"><strong>Do you have any questions about how to start selling your lunchboxes?</strong></h3>' +
@@ -17,8 +17,6 @@ function sellview() {
         '<button id="faqBtn">FAQs</button>' +
         '</div>');
 
-
-    // Attach a click event to the "Sell" button
     $("#sellBtn").off().on('click', function (e) {
         console.log('Sell button clicked');
         showSellForm();
@@ -36,8 +34,34 @@ function showSellForm() {
     $(".sellcontainerjs").load("sell.html #sellForm", function () {
         handleAllergies();
         handleProteins();
-        //nedanför anrop finns i ajax.js, "function(response){}" för att se om det var lyckat
+
+        //nedanför ajax anrop finns i ajax.js, "function(response){}" för att se om det var lyckat
         //postAd(userID, dishName, cookDate, imagePath, description, quantity, portionPrice, protein, allergy, function(response){});
+        
+        
+        $("#submitCreateAd").on('click', function (e) {
+            console.log("create ad button pressed")
+
+            //check if logged in 
+            var authData = JSON.parse(sessionStorage.getItem('auth'));
+            if (authData && authData.token != null) {
+                //fetch userID from sessionstorage
+                if(authData.user){
+                    var userID = authData.user._id;
+
+                    //fetch all input and check 
+                    var dishName = $("#lunchboxTitle").val();
+                    var cookDate = $("#lunchboxMadeDate").val();
+
+                    //call the postAd() in ajax.js
+                    //redirict or show the user somehow they have sucessfully posted an ad
+                }else{
+
+                }
+            }else{
+                loadLogInContent();
+            }
+        });
 });
     
     $("#SellQuestionsContainer").hide();
@@ -48,7 +72,7 @@ function handleProteins(){
             console.log(proteins)
             $.each(proteins, function(index, protein){
                 var proteinHTML = createProteinHtml(protein);
-                $("#allergensList").append(proteinHTML);
+                $("#proteinList").append(proteinHTML);
             })
         }else{
             $("#proteinList").append("<h1>Ops, something went wrong please try again</h1>");
@@ -56,13 +80,15 @@ function handleProteins(){
     })
 }
 
-function createProteinHtml(protein){
-    var html = '<label class="proteinContainer">' + protein.type +
-    '<input type="checkbox" id="' + protein.type.toLowerCase() +
-    '" name="protein" value="' + protein.type + '">' +
-    '<span class="checkmark"></span>' +
-    '</label>';
-return html;
+function createProteinHtml(protein) {
+    const id = protein.type.toLowerCase() + (protein.type === 'Other' ? `_${protein.source.toLowerCase()}` : '');
+    const value = protein.type === 'Other' ? `${protein.type} ${protein.source}` : protein.type;
+
+    return `
+        <label class="proteinContainer">${protein.type}${protein.type === 'Other' ? ` ${protein.source}` : ''}
+            <input type="checkbox" id="${id}" name="protein" value="${value}">
+            <span class="checkmark"></span>
+        </label>`;
 }
 
 function handleAllergies(){
@@ -86,5 +112,8 @@ function createAllergyHtml(allergy){
                '</label>';
     return html;
 }
+
+
+
 
 
