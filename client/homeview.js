024ffa9a8c857767775b44d45ddcf065c0f2be96
louer141 <louer141@su17-111.ad.liu.se>
2024-04-12 15:16:03 +0200

    Trying to fix mistakes

--- client/homeview.js
+++ client/homeview.js
@@ -5,25 +5,27 @@ function homeview() {
   $('.homeviewContainer').append('<img src="' + homeviewImageUrl + '" alt="homeview" class="homeviewImage">');
   $('.homeviewContainer').append('<h2 class="foodNearMeTitle">Food near me</h2>');
   // Embed Google Map
- var mapIframe = document.createElement('iframe');
- mapIframe.src = "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d16726.078168885902!2d15.57146175!3d58.3974506!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1szh-TW!2sse!4v1712847287937!5m2!1szh-TW!2sse";
- mapIframe.classList.add('google-map'); // Add a class to the iframe element
- //mapIframe.width = "1920"; // Set your desired width
- mapIframe.height = "450"; // Set your desired height
- mapIframe.style.border = "0";
- mapIframe.allowfullscreen = true;
- mapIframe.loading = "lazy";
- mapIframe.referrerpolicy = "no-referrer-when-downgrade";
- $('.homeviewContainer').append(mapIframe);
-
+  var mapIframe = document.createElement('iframe');
+  mapIframe.src = "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d16726.078168885902!2d15.57146175!3d58.3974506!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1szh-TW!2sse!4v1712847287937!5m2!1szh-TW!2sse";
+  mapIframe.classList.add('google-map'); // Add a class to the iframe element
+  //mapIframe.width = "1920"; // Set your desired width
+  mapIframe.height = "450"; // Set your desired height
+  mapIframe.style.border = "0";
+  mapIframe.allowfullscreen = true;
+  mapIframe.loading = "lazy";
+  mapIframe.referrerpolicy = "no-referrer-when-downgrade";
+  $('.homeviewContainer').append(mapIframe);
   $('.homeviewContainer').append('<div class="adContainer">' + '</div>');
 
+
   $('.homeviewContainer').append('<div id="filter-container" class="filter-container"></div>');
+
   $('.homeviewContainer').append('<div id="content-container" class="content-container"></div>');
 
-  $('#filter-container').load('filter.html', function () {
-  });
+  /*$('#filter-container').load('filter.html', function () {
+  });*/
 
+  /* änmdradwe namn*/
   getAds(function (cardData) {
     if (cardData) {
       $.each(cardData, function (index, card) {
@@ -31,7 +33,7 @@ function homeview() {
         $('.adContainer').append(cardHtml);
         getUser(card.sellerID, function (seller) {
           console.log(seller);
-          var modal = foodAdModal(card, index, seller);
+          var modal = homeviewModal(card, index, seller);
           $('.homeviewContainer').append(modal);
         });
       });
@@ -40,11 +42,12 @@ function homeview() {
   });
   handleclicks();
 }
+  /* änmdradwe namn*/
 
 function handleclicks() {
   $('.homeviewContainer').on('click', '.buy-btn', function () {
     var modalIndex = $(this).data('target').split('_')[1];
-    $('#foodadmodal_' + modalIndex).modal('show');
+    $('#homeviewmodal_' + modalIndex).modal('show');
   });
   $('.homeviewContainer').on('click', '.close-btn', function () {
     $(this).closest('.modal').modal('hide');
@@ -61,7 +64,7 @@ function createCard(index, card) {
     '<h5 class="ADcard-title">' + card.dishName + '</h5>' +
     '<p class="ADcard-text">' + card.description + '</p>' +
     '<p class="ADcard-price">Price: ' + card.portionPrice + ' kr/pc</p>' +
-    '<button type="button" class="btn btn-primary buy-btn" data-toggle="modal" data-target="#myModal_' + index + '">View</button>' +
+    '<button type="button" class="btn btn-primary buy-btn" data-toggle="modal" data-target="#homeviewmodal_' + index + '">View</button>' +
     '</div>' +
     '</div>';
   return cardHtml;
@@ -93,7 +96,7 @@ function createCard(index, card) {
   return modalHtml;
 }*/
 /*EDITED CODE TO FIT CSS */
-function foodAdModal(card, index, seller) {
+/*function foodAdModal(card, index, seller) {
   var modalHtml = '<div class="modal fade" id="foodadmodal_' + index + '" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">' +
     '<div class="modal-dialog">' +
     '<div class="modal-content">' +
@@ -117,7 +120,34 @@ function foodAdModal(card, index, seller) {
     '</div>';
 
   return modalHtml;
+}*/
+
+function homeviewModal(card, index, seller) {
+  var modalHtml = '<div class="modal fade" id="homeviewmodal_' + index + '" tabindex="-1" aria-labelledby="homeviewModalLabel" aria-hidden="true">' +
+    '<div class="modal-dialog">' +
+    '<div class="modal-content">' +
+    '<div class="modal-header">' +
+    '<h5 class="modal-title" id="homeviewModalLabel">' + card.dishName + '</h5>' +
+    '<button type="button" class="close close-btn" data-bs-dismiss="modal" aria-label="Close"></button>' +
+    '</div>' +
+    '<div class="modal-body">' +
+    '<img src="Images/TestFoodImage.jpg" class="modal-img" alt="Food Image">' +
+    '<p class="modal-description">' + card.description + '</p>' +
+    '<p><span class="modal-label">Quantity:</span> <span class="modal-quantity">' + card.quantity + '</span></p>' +
+    '<p><span class="modal-label">Price:</span> <span class="modal-price">' + card.portionPrice + ' kr/pc</span></p>' +
+    '<p><span class="modal-label">Seller:</span> <span class="modal-seller">' + seller.name + '</span></p>' +
+    '</div>' +
+    '<div class="modal-footer">' +
+    '<button type="button" class="btn btn-secondary close-btn" data-bs-dismiss="modal">Close</button>' +
+    '<button type="button" class="btn btn-primary add-to-cart-btn" onclick="addToCart(\'' + card._id + '\', ' + index + ')">Add to Shopping Cart</button>' +
+    '</div>' +
+    '</div>' +
+    '</div>' +
+    '</div>';
+
+  return modalHtml;
 }
+
 
 
 function addtocart(id, index) {
@@ -133,5 +163,3 @@ function addtocart(id, index) {
   });
   $('#foodadmodal_' + index).modal('hide');
 }
-
-
