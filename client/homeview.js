68308f4a0018e509b9842f4d7aef983c945e8cb1
louer141 <louer141@su03-104.ad.liu.se>
2024-04-16 12:22:23 +0200

    Problem with cardAds. Will try to solve later

--- client/homeview.js
+++ client/homeview.js
@@ -8,59 +8,63 @@ function homeview() {
   var mapIframe = document.createElement('iframe');
   mapIframe.src = "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d16726.078168885902!2d15.57146175!3d58.3974506!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1szh-TW!2sse!4v1712847287937!5m2!1szh-TW!2sse";
   mapIframe.classList.add('google-map'); // Add a class to the iframe element
-  //mapIframe.width = "1920"; // Set your desired width
-  mapIframe.height = "450"; // Set your desired height
+  mapIframe.height = "450";
   mapIframe.style.border = "0";
   mapIframe.allowfullscreen = true;
   mapIframe.loading = "lazy";
   mapIframe.referrerpolicy = "no-referrer-when-downgrade";
   $('.homeviewContainer').append(mapIframe);
+  /*End of Google Map */
   $('.homeviewContainer').append('<div class="adContainer">' + '</div>');
-
-
+  /*Filter content*/
   $('.homeviewContainer').append('<div id="filter-container" class="filter-container"></div>');
-
-  /*$('.homeviewContainer').append('<div id="content-container" class="content-container"></div>');*/
-
+  $('.homeviewContainer').append('<div id="content-container" class="content-container"></div>');
   $('#filter-container').load('filter.html', function () {
   });
 
-  /* änmdradwe namn*/
-  getAds(function (cardData) {
-    if (cardData) {
-      $.each(cardData, function (index, card) {
+  if(!ads){
+    getAds(function(cardData){
+      handleCardData(cardData)
+    });
+  }else{
+    handleCardData(ads)
+  }
+      handleclicks();
+  }
+  
+  function handleCardData(cardData){
+    if(cardData){
+      $.each(cardData, function(index, card) {
         var cardHtml = createCard(index, card);
         $('.adContainer').append(cardHtml);
-        getUser(card.sellerID, function (seller) {
+        getUser(card.sellerID, function(seller){
           console.log(seller);
-          var modal = foodAdModal(card, index, seller); /*homeViewModal */
+          var modal = foodAdModal(card, index, seller);
           $('.homeviewContainer').append(modal);
         });
       });
-    } else {
+    }else{
+      console.log("no ads exist")
     }
-  });
-  handleclicks();
-}
-/* änmdradwe namn*/
-
-function handleclicks() {
-  $('.homeviewContainer').on('click', '.buy-btn', function () {
-    var modalIndex = $(this).data('target').split('_')[1];
-    $('#foodadmodal_' + modalIndex).modal('show');
-  });
-  $('.homeviewContainer').on('click', '.close-btn', function () {
-    $(this).closest('.modal').modal('hide');
-  });
-  $('.homeviewContainer').on('click', '.btn btn-secondary close-btn', function () {
-    $(this).closest('.modal').modal('hide');
-  });
-}
+  }
+  
+  function handleclicks(){
+    $('.homeviewContainer').on('click', '.buy-btn', function() {
+      var modalIndex = $(this).data('target').split('_')[1];
+      $('#foodadmodal_' + modalIndex).modal('show');
+    });
+    $('.homeviewContainer').on('click', '.close-btn', function() {
+      $(this).closest('.modal').modal('hide');
+    });
+    $('.homeviewContainer').on('click', '.btn btn-secondary close-btn', function() {
+      $(this).closest('.modal').modal('hide');
+    });
+  }  
 
 function createCard(index, card) {
   var cardHtml = '<div class="cardAD">' +
     '<div class="card-body">' +
-    '<img src="Images/TestFoodImage.jpg" class="card-img-top" alt="...">' +
+    //'<img src="Images/TestFoodImage.jpg" class="card-img-top" alt="...">' +
     '<h5 class="ADcard-title">' + card.dishName + '</h5>' +
     '<p class="ADcard-text">' + card.description + '</p>' +
     '<p class="ADcard-price">Price: ' + card.portionPrice + ' kr/pc</p>' +
@@ -70,7 +74,7 @@ function createCard(index, card) {
   return cardHtml;
 }
 
-function foodAdModal(card, index, seller) {
+/*function foodAdModal(card, index, seller) {
   var modalHtml = '<div class="modal fade" id="foodadmodal_' + index + '" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">' +
     '<div class="modal-dialog">' +
     '<div class="modal-content">' +
@@ -78,7 +82,7 @@ function foodAdModal(card, index, seller) {
     '<h4 class="cardAd-modal-title" id="exampleModalLabel">' + card.dishName + '</h4>' +
     '</div>' +
     '<div class="cardAd-modal-body">' +
-    '<img src="Images/TestFoodImage.jpg" class="modal-img" alt="Food Image">' +
+    //'<img src="Images/TestFoodImage.jpg" class="modal-img" alt="Food Image">' +
     '<p class="cardAd-description-text">' + card.description + '</p>' +
     '<p><span class="quantity-modal-label">Quantity:</span> ' + card.quantity + '</p>' +
     '<p><span class="price-modal-label">Price:</span> ' + card.portionPrice + ' kr/pc</p>' +
@@ -93,7 +97,35 @@ function foodAdModal(card, index, seller) {
     '</div>';
 
   return modalHtml;
+}*/
+
+/*NY från backend grabbarna */
+function foodAdModal(card, index, seller){
+  var modalHtml = '<div class="modal fade" id="foodadmodal_' + index + '" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">' +
+      '<div class="modal-dialog">' +
+      '<div class="modal-content">' +
+      '<div class="modal-header">' +
+      '<h5 class="cardAd-modal-title" id="exampleModalLabel">' + card.dishName + '</h5>' +
+      '<button type="adbutton" class="close-btn" data-bs-dismiss="modal" aria-label="Close"></button>' +
+      '</div>' +
+      '<div class="cardAd-modal-body">' +
+      //'<img src="' + card.imagePath + '" class="modal-img" alt="Food Image">' +
+      '<p class="cardAd-description-text">' + card.description + '</p>' +
+      '<p><span class="cookDate-modal-label">Cook Date:</span> ' + card.cookDate + '</p>' +
+      '<p><span class="quantity-modal-label">Quantity:</span> ' + card.quantity + '</p>' +
+      '<p><span class="price-modal-label">Price:</span> ' + card.portionPrice + ' kr/pc</p>' +
+      '<p><span class="seller-modal-label">Seller:</span> ' + seller.name + '</p>' +
+      '</div>' +
+      '<div class="modal-footer">' +
+      '<button type="adbutton" class="btn btn-secondary close-btn" data-dismiss="modal">Close</button>' +
+      '<button type="adbutton" class="btn btn-primary add-to-cart-btn" onclick="addtocart(\'' + card._id + '\', ' + index + ')">Add to Shopping Cart</button>' +
+      '</div>' +
+      '</div>' +
+      '</div>' +
+      '</div>';
+  return modalHtml;
 }
+
 
 function addtocart(id, index) {
   getAd(id, function (ad) {
