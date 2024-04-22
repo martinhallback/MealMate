fb8da1350fa6d6d5241a5417ca0d4f568f3d1067
PontusAL <ponat404@student.liu.se>
2024-04-22 08:49:22 +0200

    nu kan man adda admins också

--- server/routes/admin.py
+++ server/routes/admin.py
@@ -36,7 +36,7 @@ def admin_view():
             print(e)
     return jsonify([item.serialise_client() for item in users]), 200
 
-@bp.route('/admin/update/<string:id>', methods = ['PUT'])
+@bp.route('/admin/verify/<string:id>', methods = ['PUT'])
 @jwt_required()
 def verify_user(id):
     current_user = get_jwt_identity()
@@ -47,6 +47,23 @@ def verify_user(id):
     except:
         return 400
     update_result = db['user'].update_one({'_id' : obj_id}, {'$set' : {'isVerified' : True}})
+    if update_result.modified_count == 1:
+        return jsonify({'success' : "user is now verified"}), 200
+    else:
+        return jsonify({'error' : "User was not found, or already verified"}), 400
+
+
+@bp.route('/admin/admin/<string:id>', methods = ['PUT'])
+@jwt_required()
+def verify_user(id):
+    current_user = get_jwt_identity()
+    if not verify_admin(current_user):
+        return jsonify({'error' : "You don't have the autority for this request"}), 403
+    try:
+        obj_id = ObjectId(id)
+    except:
+        return 400
+    update_result = db['user'].update_one({'_id' : obj_id}, {'$set' : {'isAdmin' : True}})
     if update_result.modified_count == 1:
         return jsonify({'success' : "user is now verified"}), 200
     else:
